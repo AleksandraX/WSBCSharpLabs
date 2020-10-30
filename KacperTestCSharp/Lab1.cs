57cb072a80f1532fe4b2662408329d06e769988e
Aleksandra 🐱‍🏍 <ola.czerwinska@dnvgl.com>
2020-10-30 14:40:41 +0100

    Initial

---
+++ KacperTestCSharp/Lab1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+
+namespace KacperTestCSharp
+{
+    //Zadanie 1
+    public class Samochod
+    {
+        private string marka, model;
+        private int iloscDrzwi;
+        private double pojemnoscSilnika, srednieSpalanie;
+        private static int iloscSamochodow = 0;
+
+
+        public string Marka
+        {
+            get { return Marka; }
+            set { marka = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int IloscDrzwi
+        {
+            get { return iloscDrzwi; }
+            set { iloscDrzwi = value; }
+        }
+
+        public double SrednieSpalanie
+        {
+            get { return srednieSpalanie; }
+            set { srednieSpalanie = value; }
+        }
+
+        public double PojemnoscSilnika
+        {
+            get { return pojemnoscSilnika; }
+            set { pojemnoscSilnika = value; }
+        }
+
+        public Samochod()
+        {
+            marka = "nieznana";
+            model = "nieznany";
+            iloscDrzwi = 0;
+            pojemnoscSilnika = 0.0;
+            srednieSpalanie = 0.0;
+        }
+
+
+        public Samochod(string marka_, string model_, int iloscDrzwi_, double pojemnoscSilnika_, double srednieSpalanie_)
+        {
+            marka = marka_;
+            model = model_;
+            iloscDrzwi = iloscDrzwi_;
+            pojemnoscSilnika = pojemnoscSilnika_;
+            srednieSpalanie = srednieSpalanie_;
+            iloscSamochodow = iloscSamochodow + 1;
+        }
+
+
+        private double ObliczSpalanie(double dlugoscTrasy)
+        {
+            return (srednieSpalanie * dlugoscTrasy) / 100.0;
+        }
+
+        public double ObliczKosztPrzejazdu(double dlugoscTrasy, double cenaPaliwa)
+        {
+            double x = ObliczSpalanie(dlugoscTrasy);
+            return x * cenaPaliwa;
+        }
+
+        public void WypiszInfo()
+        {
+            Console.WriteLine("Marka: " + marka);
+            Console.WriteLine("Model: " + model);
+            Console.WriteLine("Ilość Drzwi: " + iloscDrzwi);
+            Console.WriteLine("Pojemnosc Silnika: " + pojemnoscSilnika);
+            Console.WriteLine("Srednie Spalanie: " + srednieSpalanie);
+        }
+
+        public static void WypiszIloscSamochodow()
+        {
+            Console.WriteLine("Ilość Samochodów: " + iloscSamochodow);
+        }
+    }
+
+
+
+
+//Zadanie 2
+public class Garaz
+    {
+        private string _adres;
+        private int pojemnosc;
+        private int liczbaSamochodow = 0;
+        private Samochod[] samochody;
+
+
+
+        public string Addres { get; set; }
+
+        public int Pojemnosc
+        {
+            get  => pojemnosc; 
+            set
+            {
+                pojemnosc = value;
+                samochody = new Samochod[pojemnosc];
+            }
+        }
+
+        public Garaz()
+        {
+            Addres = "nieznany";
+            pojemnosc = 0;
+            samochody = null;
+        }
+
+        public Garaz(string adres, int pojemnosc)
+        {
+            this.Addres = adres;
+            this.Pojemnosc = pojemnosc;
+            samochody = new Samochod[pojemnosc];
+        }
+
+        public void WprowadzSamochod(Samochod samochod)
+        {
+
+            if (pojemnosc == liczbaSamochodow)
+            {
+                Console.WriteLine("Garaż jest pełny");
+            }
+            else
+            {
+                samochody[liczbaSamochodow] = samochod;
+                liczbaSamochodow++;
+            }
+
+        }
+
+        public Samochod WyprowadzSamochod()
+        {
+
+            if (liczbaSamochodow == 0)
+            {
+                Console.WriteLine("Garaz jest pusty");
+            }
+            else
+            {
+                liczbaSamochodow--;
+                samochody[liczbaSamochodow] = null;
+            }
+            return samochody[liczbaSamochodow];
+        }
+
+        public void WypiszInfo()
+        {
+            Console.WriteLine("Adres Garażu:" + Addres);
+            Console.WriteLine("Pojemność Garażu:" + pojemnosc);
+            Console.WriteLine("Liczba Samochodów w garażu:" + liczbaSamochodow);
+
+            for (int i = 0; i < liczbaSamochodow; i++)
+            {
+                samochody[i].WypiszInfo();
+            }
+        }
+    }
+}
---
+++ KacperTestCSharp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace KacperTestCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Meth1();
+            Console.ReadKey();
+        }
+
+
+        public static void Meth1()
+        {
+            Samochod s1 = new Samochod();
+            s1.WypiszInfo();
+            s1.Marka = "Fiat";
+            s1.Model = "126p";
+            s1.IloscDrzwi = 2;
+            s1.PojemnoscSilnika = 650;
+            s1.SrednieSpalanie = 6.0;
+            s1.WypiszInfo();
+            Samochod s2 = new Samochod("Syrena", "105", 2, 800, 7.6);
+            s2.WypiszInfo();
+            double kosztPrzejazdu = s2.ObliczKosztPrzejazdu(30.5, 4.85);
+            Console.WriteLine("Koszt przejazdu: " + kosztPrzejazdu);
+            Samochod.WypiszIloscSamochodow();
+            
+        }
+
+        public static void Meth2()
+        {
+            Samochod s1 = new Samochod("Fiat", "126p", 2, 650, 6.0);
+            Samochod s2 = new Samochod("Syrena", "105", 2, 800, 7.6);
+            Garaz g1 = new Garaz();
+            g1.Addres = "ul. Garażowa 1";
+            g1.Pojemnosc = 1;
+            Garaz g2 = new Garaz("ul. Garażowa 2", 2);
+            g1.WprowadzSamochod(s1);
+            g1.WypiszInfo();
+            g1.WprowadzSamochod(s2);
+            g2.WprowadzSamochod(s2);
+            g2.WprowadzSamochod(s1);
+            g2.WypiszInfo();
+            g2.WyprowadzSamochod();
+            g2.WypiszInfo();
+            g2.WyprowadzSamochod();
+            g2.WyprowadzSamochod();
+        }
+    }
+}
